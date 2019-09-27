9d0863e6c9a3af79b999408824d0148d3ea2728c
afzalmadeveloper <afzalmda786@gmail.com>
2019-09-27 17:14:01 +0530

    notepad functionality

--- script.js
+++ script.js
@@ -6,14 +6,42 @@
 //     code: 'document.body.style.backgroundColor="red"'
 //   });
 // });
-document.addEventListener('DOMContentLoaded', function () {
-  var link = document.getElementById('link');
-  link.addEventListener('click', function () {
-    loadNotepad();
-  });
-});
 
-function loadNotepad() {
-  document.body.innerHTML = "<textarea>text goes here</textarea>"
+// tinymce.init({
+//   selector: '#textarea',
+//   toolbar: 'undo redo | styleselect | bold italic | link image',
+
+//   plugins: 'advlist autolink link image lists charmap print preview'
+
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var link = document.getElementById('link');
+//   link.addEventListener('click', function () {
+//     loadNotepad();
+//   });
+// });
+
+// function loadNotepad() {
+//   document.body.innerHTML = `<textarea>text goes here</textarea>
+//   <button class=green>bold</button>`
+// }
+function bold() {
+  console.log("hai")
+  document.getElementsByClassName("button1").style.fontWeight = "bold";
 }
 
+document.getElementById('link').addEventListener('click', function () {
+  var notepad = document.createElement('textarea'); // INPUT also works
+  notepad.setAttribute('type', 'text');
+  notepad.setAttribute('class', 'notepad');
+  document.body.appendChild(notepad);
+
+  var bold = document.createElement('button');
+  bold.setAttribute('class', 'button1');
+  bold.setAttribute('name', 'bold');
+  bold.setAttribute('onclick', 'bold()');
+  bold.innerHTML = 'BOLD';
+  document.body.appendChild(bold);
+  bold();
+});
